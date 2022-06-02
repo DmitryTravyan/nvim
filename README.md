b65b340e5ba92d95efc9f7f1ad644bc5ba1c8bf4
dmitry.travyan <dmitry.travyan@picodata.io>
2022-06-02 14:15:17 +0300

    fix: update nvim_tree

---
+++ README.md
@@ -0,0 +1,6 @@
+
+RHEL
+```shell
+sudo dnf install -y tmux node tmux zsh nodejs
+git clone https://github.com/ryanoasis/nerd-fonts ; cd nerd-fonts ; ./setup.sh
+```
--- lua/conf/nvimtree.lua
+++ lua/conf/nvimtree.lua
@@ -1,26 +1,3 @@
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
-  },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-  },
-}
-
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
   return
@@ -41,6 +18,30 @@ nvim_tree.setup {
       "startify",
       "dashboard",
       "alpha",
+    },
+    renderer = {
+        icons = {
+            glyphs = {
+                default = "",
+                symlink = "",
+                git = {
+                    unstaged = "",
+                    staged = "S",
+                    unmerged = "",
+                    renamed = "➜",
+                    deleted = "",
+                    untracked = "U",
+                    ignored = "◌",
+                },
+                folder = {
+                    default = "",
+                    open = "",
+                    empty = "",
+                    empty_open = "",
+                    symlink = "",
+                },
+            },
+        },
     },
     --auto_close = true,
     open_on_tab = false,
