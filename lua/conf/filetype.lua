7916bcec6dcc2c0f18b96d5c783956eb982c371c
dmitry.travyan <dtrser@gmail.com>
2022-11-29 02:07:43 +0300

    fix: templates in txt files

--- lua/conf/filetype.lua
+++ lua/conf/filetype.lua
@@ -1,26 +1,44 @@
+local function find_gompl_pattern()
+    return string.match(
+        table.concat(vim.api.nvim_buf_get_lines(tonumber(vim.fn.bufnr("%")), 0, -1, false), " "),
+        "{{.+}}"
+    )
+end
+
 require("filetype").setup({
-	overrides = {
-		function_extensions = {
-			["yml"] = function()
-				if vim.fn.search("{{.+}}") then
-					vim.bo.filetype = "gotmpl"
-				end
-			end,
-			["yaml"] = function()
-				if vim.fn.search("{{.+}}") then
-					vim.bo.filetype = "gotmpl"
-				end
-			end,
-			["tpl"] = function()
-				if vim.fn.search("{{.+}}") then
-					vim.bo.filetype = "gotmpl"
-				end
-			end,
-		},
-		function_literal = {
-			Brewfile = function()
-				vim.lsp.diagnostic.disable()
-			end,
-		},
-	},
+    overrides = {
+        function_extensions = {
+            ["yml"] = function()
+                if find_gompl_pattern() then
+                    vim.bo.filetype = "gotmpl"
+                else
+                    vim.bo.filetype = "yaml"
+                end
+            end,
+            ["yaml"] = function()
+                if find_gompl_pattern() then
+                    vim.bo.filetype = "gotmpl"
+                else
+                    vim.bo.filetype = "yaml"
+                end
+            end,
+            ["tpl"] = function()
+                if find_gompl_pattern() then
+                    vim.bo.filetype = "gotmpl"
+                end
+            end,
+            ["txt"] = function()
+                if find_gompl_pattern() then
+                    vim.bo.filetype = "gotmpl"
+                else
+                    vim.bo.filetype = "txt"
+                end
+            end,
+        },
+        function_literal = {
+            Brewfile = function()
+                vim.lsp.diagnostic.disable()
+            end,
+        },
+    },
 })
