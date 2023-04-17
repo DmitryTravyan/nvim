4270933a6ee544095c8a598928ab381e03a11b03
dmitry.travyan <dmitry.travyan@picodata.io>
2023-04-17 18:01:22 +0300

    fix: handlers

--- lua/conf/lsp/handlers.lua
+++ lua/conf/lsp/handlers.lua
@@ -52,22 +52,6 @@ M.setup = function()
     )
 end
 
-local function lsp_highlight_document(client)
-    -- Set autocommands conditional on server_capabilities
-    if client.server_capabilities.document_highlight then
-        vim.api.nvim_exec(
-            [[
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]]       ,
-            false
-        )
-    end
-end
-
 local function lsp_keymaps(bufnr)
     local opts = { noremap = true, silent = true }
     vim.api.nvim_buf_set_keymap(
@@ -173,8 +157,8 @@ M.on_attach = function(client, bufnr)
     if client.name == "tsserver" then
         client.resolved_capabilities.document_formatting = false
     end
+    client.server_capabilities.semanticTokensProvider = nil
     lsp_keymaps(bufnr)
-    lsp_highlight_document(client)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
